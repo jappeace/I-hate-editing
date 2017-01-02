0da98929ebbd8dffc94ee2ee1efd28dae846eb99
Jappie Klooster <jappieklooster@hotmail.com>
2017-01-02 23:57:19 +0100

    readme

--- editing.py
+++ editing.py
@@ -4,6 +4,7 @@ from bpy import context
 scene = context.scene
 
 path = "/home/jappie/Videos/C/"
+path = input("folder > ")
 files = os.listdir(path)
 files.sort()
 
@@ -11,24 +12,32 @@ files.sort()
 scene.sequence_editor_create()
 seqs = scene.sequence_editor.sequences
 
-last_end = 0
+last_end = 1
 for (num, file) in enumerate([f for f in files if f.endswith('.wav')]):
     filename_parts = file.split('-')
+    desired_order = filename_parts[0]
     
     seq = seqs.new_sound(
             name=str(file),
             filepath=os.path.join(path, file),
             channel=1, frame_start=last_end)
 
-    for (count, img) in enumerate([f for f in files if f.startswith(filename_parts[0]) and (f.endswith('.png') or f.endswith('.jpg'))]):
+    def isInRange(imgfilename):
+        range = imgfilename.split('-')[0].split('..')
+        if len(range) <= 1:
+            return False
+        result = int(range[0]) <= int(desired_order) <= int(range[1])
+        return result
+
+    for (count, img) in enumerate([f for f in files if (f.startswith(desired_order) or isInRange(f)) and (f.endswith('.png') or f.endswith('.jpg'))]):
         imgseq = seqs.new_image(
             name=img,
             filepath=os.path.join(path, img),
             channel=count+2,
             frame_start=last_end,
         )
-
+        imgseq.frame_final_end = seq.frame_final_end
 
     last_end = seq.frame_final_end
-# reverse if you want
-seq.use_reverse_frames = False
+
+scene.frame_end = last_end
